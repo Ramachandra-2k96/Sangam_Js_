3439ab9eee5ecffe1da80c88b107228661bebcfb
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-12-24 21:19:37 +0530

    changed

--- Calender/script.js
+++ Calender/script.js
@@ -1,20 +1,81 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var table="";
+let my_map = new Map();
 
 function start()
 {
-    table="";
-    var year = document.getElementById("input").value;
-    var holiday = document.getElementById("Dropdown").value;
-    var lang = document.getElementById("Dropdown_lang").value;
-    for(let k=0;k<12;k++)//call the function 12 times for 12 months
-    {
-        draw_calender(months[k].substring(0,3), year, lang, holiday);//Pass first 3 letters of every elements
+    var f = document.getElementById("fileinput");
+    if (f.files.length) {
+        var file_reader =new FileReader();
+        var dates =[];
+
+        file_reader.onload = function(e){
+            var content =e.target.result;
+            var lines = content.split('\n');
+            for(l of lines)
+            {
+                dates.push(l.replace('\r',''));
+            }            
+            dates.forEach(d => {
+                const [day, month, year] = d.split('-');
+                if (!my_map.has(month)) {
+                    my_map.set(month,[parseInt(day)]);
+                } else {
+                    my_map.get(month).push(parseInt(day));
+                }
+      });
+      table="";
+      console.log(my_map);
+      var year = document.getElementById("input").value;
+      if (year === "") 
+      {
+          alert("please add year first");
+          location.reload();
+      }
+      var holiday = document.getElementById("Dropdown").value;
+      var lang = document.getElementById("Dropdown_lang").value;
+      for(let k=0;k<12;k++)//call the function 12 times for 12 months
+      {
+        if(my_map.has(months[k].substring(0,3)))
+        {
+            draw_calender(months[k].substring(0,3), year, lang, holiday , my_map.get(months[k].substring(0,3)));//Pass first 3 letters of every elements
+        }
+        else
+        {
+            draw_calender(months[k].substring(0,3), year, lang, holiday , []);//Pass first 3 letters of every elements
+        } 
+      }
+      document.getElementById("calender").innerHTML =table; //grab calender ID and create all the tables there
+    };
+        file_reader.readAsText(f.files[0]);
     }
-    document.getElementById("calender").innerHTML =table; //grab calender ID and create all the tables there
+    else
+    {
+        table="";
+        var year = document.getElementById("input").value;
+      if (year === "") 
+      {
+          alert("please add year first");
+          location.reload();
+      }
+      var holiday = document.getElementById("Dropdown").value;
+      var lang = document.getElementById("Dropdown_lang").value;
+      for(let k=0;k<12;k++)//call the function 12 times for 12 months
+      {
+        if(my_map.has(months[k].substring(0,3)))
+        {
+            draw_calender(months[k].substring(0,3), year, lang, holiday , my_map.get(months[k].substring(0,3)));//Pass first 3 letters of every elements
+        }
+        else
+        {
+            draw_calender(months[k].substring(0,3), year, lang, holiday , []);//Pass first 3 letters of every elements
+        } 
+      }
+      document.getElementById("calender").innerHTML =table; //grab calender ID and create all the tables there
+    }
 }
 // Function to draw calender
-function draw_calender(mon, year, lang, holiday)
+function draw_calender(mon, year, lang, holiday, dates)
 {
     table+="<table border ='2'><thead><th colspan='7'>";
     var start_date = new Date("01-"+mon+"-"+year);
@@ -23,12 +84,11 @@ function draw_calender(mon, year, lang, holiday)
     let day_element=0;
     let i=0;
     table+=start_date.toLocaleDateString(lang,{ month: 'long' })+", "+year+"</th></thead><tbody><tr>"; // write month name as a heading
-    var days_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    for(var d in days_of_week) // write all the week names 
+    for(let i=0;i<7;i++) // write all the week names 
     {
-        let date = new Date(2023, 0, 1 + d - 2);
+        let date = new Date(2023, 0, 1 + i );
         let dow = date.toLocaleDateString(lang,{weekday:'short'});
-        if(d==holiday){
+        if(i==holiday){
             table+="<td class='sunday weekdays'>"+ dow +"</td>";
         }
         else{
@@ -45,7 +105,7 @@ function draw_calender(mon, year, lang, holiday)
                 if(day_element<last_date.getDate())//check if the date is within the range of last date
                 {
                     day_element++;
-                    if(j==holiday){// j=0 means it's a sunday so give the text red color
+                    if(j==holiday || (dates && dates.includes(day_element))){// j=0 means it's a sunday so give the text red color and if it is in the list of holidays
                         table+="<td class='sunday'>"+day_element+"</td>";
                     }
                     else{   //else normal color
